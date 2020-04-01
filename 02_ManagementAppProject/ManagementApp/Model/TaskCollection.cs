fb0eb4bf735954a71780de9a5fe73c997074c53a
Adam <a.baczewski@interia.eu>
2020-04-01 11:38:11 +0200

    Utworzenie projektu aplikacji desktopowej WPF

---
+++ 02_ManagementAppProject/ManagementApp/Model/TaskCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ManagementApp.Model
+{
+    public class TaskCollection
+    {
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+        public List<Task> Tasks { get; set; }
+
+        public TaskCollection()
+        {
+            this.Tasks = new List<Task>();
+            Name = "Jakiś tam zbiór zadań";
+        }
+    }
+}
