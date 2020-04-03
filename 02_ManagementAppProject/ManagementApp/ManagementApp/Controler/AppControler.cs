a9d0493b77727b28c21296944e8351ddb1eee85c
łUkAsZ gŁoWaCkI <poiqwe97@gmail.com>
2020-04-03 19:18:43 +0200

    Kolejne funkcjonalnosci obsluugi bazy danych

--- 02_ManagementAppProject/ManagementApp/ManagementApp/Controler/AppControler.cs
+++ 02_ManagementAppProject/ManagementApp/ManagementApp/Controler/AppControler.cs
@@ -41,43 +41,127 @@ namespace ManagementApp.Controler
 
 		public static void DeleteCollection(int collection_ID)
 		{
+			using (var context = new AppDataBase())
+			{
+				var findedTaskCollection = context.TaskCollection.Where(s => s.TaskCollection_ID == collection_ID).FirstOrDefault<TaskCollection>();
+				context.TaskCollection.Remove(findedTaskCollection);
+				context.SaveChanges();
+			}
 		}
 
+		/*
+		 *  var student = ctx.Students
+                  .Where(s => s.StudentName == name)
+                  .FirstOrDefault<Student>()
+		 */
 		public static void DeleteTask(int collection_ID, int task_ID)
 		{
+			using (var context = new AppDataBase())
+			{
+				var findedTask = context.Task.Where(s => s.Task_ID == task_ID).FirstOrDefault<Task>();
+				context.Task.Remove(findedTask);
+				context.SaveChanges();
+			}
 		}
 		public static void DeletePoint(int task_ID, int point_ID)
 		{
-		}
-
-		public static void ChangeCollectionProperties(int collection_ID ) //???
-		{
-			//Jak to zgrabnie rozwiązać?
-		}
-
-		public static void ChangeTaskProperties(int collection_ID, int task_ID) //???
-		{
-			//Jak to zgrabnie rozwiązać?
-		}
-		public static void ChangePointProperties(int collection_ID, int task_ID, int point_ID) //???
-		{
-			//Jak to zgrabnie rozwiązać?
-		}
+			using(var context = new AppDataBase())
+			{
+				var findedPoint = context.Point.Where(s => s.Point_ID == point_ID).FirstOrDefault<Point>();
+				context.Point.Remove(findedPoint);
+				context.SaveChanges();
 
 
-
-		public static List<TaskCollection> GetCollectionsList()
+			}
+		}
+		public static void ChangeCollectionProperties(int collection_ID, TaskCollection newCollection) //???
 		{
-			return new List<TaskCollection>();
+			using (var context = new AppDataBase())
+			{
+				var findedTaskCollection = context.TaskCollection.Where(s => s.TaskCollection_ID == collection_ID).FirstOrDefault<TaskCollection>();		
+				findedTaskCollection.Name = findedTaskCollection.Name;
+				findedTaskCollection.Description = findedTaskCollection.Description;
+				context.SaveChanges();
+			}
 		}
 
-		public static List<Task> GetTasksList(int collection_ID)
+		public static void ChangeTaskProperties(int task_ID, Task newTask) //???
 		{
-			return new List<Task>();
+			using (var context = new AppDataBase())
+			{
+				var findedTask = context.Task.Where(s => s.Task_ID == task_ID).FirstOrDefault<Task>();
+				findedTask.Collection_ID = newTask.Collection_ID;
+				findedTask.TaskName = newTask.TaskName;
+				findedTask.Completion= newTask.Completion;
+				findedTask.DeadLine = newTask.DeadLine;
+				findedTask.Percenttarget = newTask.Percenttarget;
+				findedTask.Description = newTask.Description;
+				context.SaveChanges();
+			}
 		}
-		public static List<Point> GetPointsList(int task_ID)
+		public static void ChangePointProperties(int point_ID, Point newPoint)
 		{
-			return new List<Point>();
+			using(var context = new AppDataBase())
+			{
+				var findedPoint = context.Point.Where(s => s.Point_ID == point_ID).FirstOrDefault<Point>();
+				findedPoint.Task_ID = newPoint.Task_ID;
+				findedPoint.Name = newPoint.Name;
+				findedPoint.TaskForToday = newPoint.TaskForToday;
+				findedPoint.ExecutionStatus = newPoint.ExecutionStatus;
+				findedPoint.CompletionDate = newPoint.CompletionDate;
+				findedPoint.DeadLineDate= newPoint.DeadLineDate;
+				findedPoint.Description = newPoint.Description;
+				context.SaveChanges();
+			}
+		}
+
+		public static TaskCollection GetTaskCollection(int collection_ID)
+		{
+			using (var context = new AppDataBase())
+			{
+				var findedTaskCollection = context.TaskCollection.Where(s => s.TaskCollection_ID == collection_ID).FirstOrDefault<TaskCollection>();				
+				return findedTaskCollection;
+			}
+		}
+		public static Task GetTask( int task_ID)
+		{
+			using (var context = new AppDataBase())
+			{
+				var findedTask = context.Task.Where(s => s.Task_ID == task_ID).FirstOrDefault<Task>();
+				return findedTask;
+			}
+		}
+		public static Point GetPoint(int point_ID)
+		{
+			using (var context = new AppDataBase())
+			{
+				var findedPoint = context.Point.Where(s => s.Point_ID == point_ID).FirstOrDefault<Point>();
+				return findedPoint;
+			}
+		}
+		public static IList<TaskCollection> GetCollectionsList()
+		{
+			using (var context = new AppDataBase())
+			{
+				IList<TaskCollection> List = context.TaskCollection.ToList<TaskCollection>();
+				return List;
+			}
+		}
+		public static IList<Task> GetTasksList()
+		{
+			using (var context = new AppDataBase())
+			{
+				IList<Task> List = context.Task.ToList<Task>();
+				return List;
+			}
+		}
+		public static IList<Point> GetPointsList()
+		{
+			using (var context = new AppDataBase())
+			{
+				IList<Point> List = context.Point.ToList<Point>();
+				return List;
+			}
 		}
 
 
